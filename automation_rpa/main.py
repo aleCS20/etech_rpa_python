78ce3b1de02e075d55aacbd741ecd83465ae7750
aleCS20 <pivodobasquete@gmail.com>
2026-05-04 10:51:09 -0300

    test case 3 e 4 finalizados

--- automation_rpa/main.py
+++ automation_rpa/main.py
@@ -1,7 +1,8 @@
 import time
 from src.tests.test_register_user import TestRegisterUser
 from src.tests.test_login_user import TestLoginUser
-
+from src.tests.test_login_user_incorret import TestLoginUserIncorrect
+from src.tests.test_logout_user import TestLogoutUser
 def orquestrar_automacoes():
     print("--- Iniciando AtividadeTest Case 1 ---")
     registro = TestRegisterUser()
@@ -14,5 +15,20 @@ def orquestrar_automacoes():
     login = TestLoginUser()
     login.executar_teste_login_sucesso()
 
+    print("⏳ Aguardando 5 segundos para o próximo Test Case ....")
+    time.sleep(5)
+
+    print("--- Iniciando Test Case 3 ---")
+    login_incoreto = TestLoginUserIncorrect()
+    login_incoreto.executar_teste_login_email_incorreto()
+
+    print("⏳ Aguardando 5 segundos para o próximo Test Case ....")
+    time.sleep(5)
+
+    print("--- Iniciando Test Case 4: Logout ---")
+    logout_test = TestLogoutUser()
+    logout_test.executar_teste_logout()
+    
 if __name__ == "__main__":
     orquestrar_automacoes()
+
